db25ac5e48b079f41d6311f722d29592ac16c096
PrismaStudiomx <webprismastudio@gmail.com>
2026-05-28 22:11:46 -0600

    cambios

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,19 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  build: {
+    // 1. Aumentamos el límite de tamaño para evitar errores de compilación
+    chunkSizeWarningLimit: 1000, 
+    
+    // 2. Optimizamos la división de archivos (esto evita que el index se corrompa)
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
